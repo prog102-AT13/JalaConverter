f4f1341f97cfc0dd6afeb1b45316d74e83dd75b8
SaulCM <saulcaspamiranda@gmail.com>
2021-04-01 12:11:54 -0400

    first commit

---
+++ src/main/java/org/fundacionjala/JalaConverter/JalaconverterApplication.java
@@ -0,0 +1,13 @@
+package org.fundacionjala.JalaConverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JalaconverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JalaconverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/fundacionjala/JalaConverter/JalaconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fundacionjala.JalaConverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JalaconverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
