2fecaa394d261d540cc4808b08f1587bb54bd33f
SaulCM <saulcaspamiranda@gmail.com>
2021-04-01 19:13:57 -0400

    add copyright and fix packages name

---
+++ src/main/java/org/fundacion/jala/converter/Controller/FileUploadController.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2021 Fundacion Jala.
+ *
+ * This software is the confidential and proprietary information of Fundacion Jala
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with Fundacion Jala
+ */
+package org.fundacion.jala.converter.Controller;
+
+import org.fundacion.jala.converter.Service.FileUploadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@RestController
+public class FileUploadController {
+
+    @Autowired
+    FileUploadService fileUploadService;
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("file")MultipartFile file, @RequestParam("metadata") boolean metadata) throws IllegalStateException, IOException {
+        //fileUploadService.uploadFile(file);
+        file.transferTo(new File("G:\\Files\\VideoUploads\\" + file.getOriginalFilename()));
+        return "uploaded";
+    }
+}
--- src/main/java/org/fundacion/jala/converter/Service/FileUploadService.java
+++ src/main/java/org/fundacion/jala/converter/Service/FileUploadService.java
@@ -1,4 +1,12 @@
-package org.fundacionjala.JalaConverter.Service;
+/**
+ * Copyright (c) 2021 Fundacion Jala.
+ *
+ * This software is the confidential and proprietary information of Fundacion Jala
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with Fundacion Jala
+ */
+package org.fundacion.jala.converter.Service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
