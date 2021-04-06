14d7be3dc92be797d4f77ac48fec039271426750
Joel Rojas <jr.bestone17@gmail.com>
2021-04-06 11:10:42 -0400

    Add log

--- src/main/java/org/fundacion/jala/converter/service/pythoncompiler/PythonCompiler.java
+++ src/main/java/org/fundacion/jala/converter/service/pythoncompiler/PythonCompiler.java
@@ -8,19 +8,24 @@
  */
 package org.fundacion.jala.converter.service.pythoncompiler;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class PythonCompiler {
-
+    private static final Logger LOGGER = LogManager.getLogger();
     /**
      * @param compilerVersion has command according version
      * @param filePath represents what file we want to compile
      * @return the result of execution in console
      */
     public String compiler(final Python compilerVersion, final String filePath) {
+        LOGGER.info("start");
         try {
+            LOGGER.info("Execute Try");
             String command = compilerVersion.getVersion() + " " + filePath;
             Process process = Runtime.getRuntime().exec(command);
             BufferedReader bufferedReaderr = new BufferedReader(new InputStreamReader(process.getInputStream()));
@@ -29,8 +34,10 @@ public class PythonCompiler {
             while((resultOfExecution = bufferedReaderr.readLine()) != null) {
                 result += resultOfExecution + "\n";
             }
+            LOGGER.info("finish");
             return result;
         } catch (IOException exception) {
+            LOGGER.error("Execute Exception" + exception.getLocalizedMessage());
             return exception.getMessage();
         }
     }
