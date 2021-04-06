ea63877813180313aee071defccfc311cc74e989
danielaSantaCruz <daniela2010.1d@gmail.com>
2021-04-06 08:18:13 -0400

    Solve checkstyle conflicts

--- src/main/java/org/fundacion/jala/converter/ConverterApplication.java
+++ src/main/java/org/fundacion/jala/converter/ConverterApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConverterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConverterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConverterApplication.class, args);
+    }
 
 }
