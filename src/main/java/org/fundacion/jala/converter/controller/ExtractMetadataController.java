4a8a65fe4ffe31c486ab4893920cd5463dbca5e9
Gustavo Huanca <gustavozha@gmail.com>
2021-04-07 15:23:00 -0400

    Add endpoint extractMetadataMoreOption

--- src/main/java/org/fundacion/jala/converter/controller/ExtractMetadataController.java
+++ src/main/java/org/fundacion/jala/converter/controller/ExtractMetadataController.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2021 Fundacion Jala.
- * 
+ * <p>
  * This software is the confidential and proprietary information of Fundacion Jala
  * ("Confidential Information"). You shall not disclose such Confidential
  * Information and shall use it only in accordance with the terms of the
@@ -40,7 +40,7 @@ public class ExtractMetadataController {
         ExtractMetadata extractMetadata = new ExtractMetadata(new File(fileToExtract), new File(fileToExport));
         extractMetadata.extractMetadata();
 
-        File file=new File(fileToExtract);
+        File file = new File(fileToExtract);
         String filename = file.getName();
         String storagePath = filename;
         String outputPath = FileStorageService.getOutputPath(filename);
@@ -50,6 +50,34 @@ public class ExtractMetadataController {
         return downloadLink;
     }
 
+    /**
+     * Endpoint for extract metadata
+     */
+    @PostMapping("/extractMetadataMoreOption")
+    public String uploadFile(@RequestParam("fileToExtract") String fileToExtract,
+                             @RequestParam("fileToExport") String fileToExport,
+                             @RequestParam("MoreInfo") Boolean isMoreInfo,
+                             @RequestParam("NameExport") String nameExport) throws IllegalStateException, IOException {
+        LOGGER.info("start");
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setFileToExtract(new File(fileToExtract));
+        objectMetadata.setFileToExport(new File(fileToExport));
+        objectMetadata.setMoreInfo(isMoreInfo);
+        objectMetadata.setNameExport(nameExport);
+
+        ExtractMetadata extractMetadata = new ExtractMetadata(objectMetadata);
+
+        extractMetadata.extractMetadata();
+
+        File file = new File(fileToExtract);
+        String filename = file.getName();
+        String storagePath = filename;
+        String outputPath = FileStorageService.getOutputPath(filename);
+        final String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
+        String downloadLink = baseUrl + "/api/download/" + filename;
+        LOGGER.info("finish");
+        return downloadLink;
+    }
 
     /**
      * Endpoint for extract metadata
