46fd9e9bb650773571b3f9a899c0c7e2dd136b44
edsonarios <edsonrios9@gmail.com>
2021-04-07 10:44:57 -0400

    fix audio converter

--- src/main/java/org/fundacion/jala/converter/ConverterApplication.java
+++ src/main/java/org/fundacion/jala/converter/ConverterApplication.java
@@ -8,7 +8,6 @@
  */
 package org.fundacion.jala.converter;
 
-import org.fundacion.jala.converter.service.AudioConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.apache.logging.log4j.Logger;
@@ -20,18 +19,7 @@ public class ConverterApplication {
 
 	public static void main(String[] args) {
 		logger.info("start");
-//		SpringApplication.run(ConverterApplication.class, args);
+		SpringApplication.run(ConverterApplication.class, args);
 		logger.info("finish");
-
-		//Usage example for the audio converter object
-
-		AudioConverter audio = new AudioConverter();
- 		audio.setFormat("mp3");//mp3,wav
- 		audio.setBitrate("320");//64,128,192,320
- 		audio.setVolume("1");//1-5 or 1-10
- 		audio.setHz("20");//20,44,48
- 		audio.audioConverter("/c/Users/Edson/Desktop/Edson/01. Jala/03. Prog102/JalaConverter/archive/Moana.mp4");
-
-
 	}
 }
