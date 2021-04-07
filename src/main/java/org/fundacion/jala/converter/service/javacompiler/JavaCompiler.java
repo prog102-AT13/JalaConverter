eb3ac0e8045caadfbf0430aa4ad2140175111352
JessickaMoya <jessicka.moya@gmail.com>
2021-04-06 23:36:03 -0400

    Added logs to JavaCompiler class

--- src/main/java/org/fundacion/jala/converter/service/javacompiler/JavaCompiler.java
+++ src/main/java/org/fundacion/jala/converter/service/javacompiler/JavaCompiler.java
@@ -8,18 +8,24 @@
  */
 package org.fundacion.jala.converter.service.javacompiler;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class JavaCompiler {
+    private static final Logger LOGGER = LogManager.getLogger();
     /**
      * @param javaVersion has the commands according to java version
      * @param filePath represents the path of the file to compile
      * @return the result of execution in console
      */
     public String javaCompiler(final JavaVersion javaVersion, final String filePath){
+        LOGGER.info("start");
         try {
+            LOGGER.info("Execute Try");
             String command = javaVersion.getCompiler() + " " + filePath + "&&" + javaVersion.getExecutor() + " " + filePath;
             ProcessBuilder processBuilder = new ProcessBuilder("cmd.exe", "/c", command);
             Process process = processBuilder.start();
@@ -29,9 +35,11 @@ public class JavaCompiler {
             while((resultOfExecution = bufferedReader.readLine()) != null){
                 result += resultOfExecution + "\n";
             }
+            LOGGER.info("finish");
             return result;
-        } catch (IOException e) {
-            return e.getMessage();
+        } catch (IOException exception) {
+            LOGGER.error("Execute Exception" + exception.getLocalizedMessage());
+            return exception.getMessage();
         }
     }
 }
