8ed261919b4300a18187d5bf2186ce9cda447939
Joel Rojas <jr.bestone17@gmail.com>
2021-04-06 23:18:58 -0400

    Integrate model with controller of python compiler

--- src/main/java/org/fundacion/jala/converter/controller/PythonCompilerController.java
+++ src/main/java/org/fundacion/jala/converter/controller/PythonCompilerController.java
@@ -8,7 +8,11 @@
  */
 package org.fundacion.jala.converter.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.fundacion.jala.converter.service.FileStorageService;
+import org.fundacion.jala.converter.service.pythoncompiler.Python;
+import org.fundacion.jala.converter.service.pythoncompiler.PythonCompiler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,6 +22,7 @@ import java.io.IOException;
 @RestController
 @RequestMapping("/api")
 public class PythonCompilerController {
+    private static final Logger LOGGER = LogManager.getLogger();
     @Autowired
     FileStorageService fileStorageService;
 
@@ -26,11 +31,20 @@ public class PythonCompilerController {
      */
     @PostMapping("/compilePython")
     public String uploadFile(@RequestParam("file") MultipartFile file, @RequestParam("version") String version) throws IllegalStateException, IOException {
+        LOGGER.info("start");
         String filename = file.getOriginalFilename();
         String storagePath = fileStorageService.uploadFile(file);
-        String outputPath = FileStorageService.getOutputPath(filename);
-        final String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
-        String downloadLink = baseUrl + "/api/download/" +  filename;
-        return downloadLink;
+        PythonCompiler pythonCompiler = new PythonCompiler();
+        LOGGER.info("finish");
+        return pythonCompiler.compiler(Python.V3, storagePath);
+    }
+
+    /**
+     * Endpoint for proving compile python
+     */
+    @GetMapping("/compilePython2")
+    public String uploadFile2(){
+        PythonCompiler pythonCompiler = new PythonCompiler();
+        return pythonCompiler.compiler(Python.V3, System.getProperty("user.dir") + "/archive/helloworld.py");
     }
 }
