2df351c8153907e3c4b826ff2d4c44d5b981de36
Gustavo Huanca <gustavozha@gmail.com>
2021-04-09 15:29:43 -0400

    Refactor setResolutionSelect method in OutputSettings class and add new ResolutionVideo class

---
+++ src/main/java/org/fundacion/jala/converter/view/converter/ResolutionVideo.java
@@ -0,0 +1,42 @@
+package org.fundacion.jala.converter.view.converter;
+
+public class ResolutionVideo {
+    private int width;
+    private int height;
+    private String name;
+
+    ResolutionVideo(String name,int width,int height ){
+        this.height=height;
+        this.width=width;
+        this.name=name;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return  height+"X"+width+"     "+name;
+    }
+}
