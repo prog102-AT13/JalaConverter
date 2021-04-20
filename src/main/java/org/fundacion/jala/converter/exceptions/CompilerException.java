7dd2af1b4dac9f9269ae0bd25b5f363486532879
JessickaMoya <jessicka.moya@gmail.com>
2021-04-20 16:32:37 -0400

    Create CompilerException class

---
+++ src/main/java/org/fundacion/jala/converter/exceptions/CompilerException.java
@@ -0,0 +1,22 @@
+package org.fundacion.jala.converter.exceptions;
+
+public class CompilerException extends PaoPaoException{
+    private final static String DEFAULT_MESSAGE = "Compile Error";
+
+    public CompilerException() {
+        super(DEFAULT_MESSAGE);
+    }
+
+    public CompilerException(String message) {
+        super(message);
+    }
+
+    public CompilerException(Throwable exception) {
+        super(DEFAULT_MESSAGE, exception);
+    }
+
+    public CompilerException(String message, Throwable exception) {
+        super(message, exception);
+    }
+
+}
