7959d9f7ea4a2cef7e684b4de5efdab523e75652
JessickaMoya <jessicka.moya@gmail.com>
2021-04-20 16:30:14 -0400

    Create main exception class

---
+++ src/main/java/org/fundacion/jala/converter/exceptions/PaoPaoException.java
@@ -0,0 +1,21 @@
+package org.fundacion.jala.converter.exceptions;
+
+public class PaoPaoException extends Exception{
+
+    public PaoPaoException() {
+        super();
+    }
+
+    public PaoPaoException(String message) {
+        super(message);
+    }
+
+    public PaoPaoException(Throwable exception) {
+        super(exception);
+    }
+
+    public PaoPaoException(String message, Throwable exception) {
+        super(message, exception);
+    }
+
+}
