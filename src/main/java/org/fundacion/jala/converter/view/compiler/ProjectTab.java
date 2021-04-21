97826983c67967e1025e24a278a75aba89ce6246
Joel Rojas <jr.bestone17@gmail.com>
2021-04-21 15:13:51 -0400

    Add change title functionality

--- src/main/java/org/fundacion/jala/converter/view/compiler/ProjectTab.java
+++ src/main/java/org/fundacion/jala/converter/view/compiler/ProjectTab.java
@@ -14,11 +14,14 @@ package org.fundacion.jala.converter.view.compiler;
 import javax.swing.JTabbedPane;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionEvent;
 
 public class ProjectTab extends JTabbedPane implements ActionListener {
     public static int contador;
@@ -34,6 +37,12 @@ public class ProjectTab extends JTabbedPane implements ActionListener {
         add(new JPanel());
         setTabComponentAt(getTabCount() - 1, button);
         contador = 0;
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                changeTitle(mouseEvent.getClickCount());
+            }
+        });
     }
 
     /**
@@ -43,21 +52,11 @@ public class ProjectTab extends JTabbedPane implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         contador ++;
-        CloseButton tabButton = new CloseButton();
         String title= "EndGame " + contador;
         CodeTextArea codeArea = new CodeTextArea();
         codeArea.setName(title);
         add(codeArea, getTabCount() - 1);
-        tabButton.setPreferredSize(new Dimension(20,20));
-        tabButton.addActionListener(e1 -> { removeTap(title);setSelectedIndex(getTabCount() - 2);});
-        JPanel pnl = new JPanel();
-        pnl.setLayout(new FlowLayout());
-        pnl.setOpaque(false);
-        JLabel label=new JLabel(title);
-        label.setFont(new Font("Barlow", 0, 11));
-        pnl.add(label);
-        pnl.add(tabButton);
-        setTabComponentAt(getTabCount() - 2, pnl);
+        setTabComponentAt(getTabCount() - 2, makeTabTitle(title));
         setSelectedIndex(getTabCount() - 2);
     }
 
@@ -84,5 +83,41 @@ public class ProjectTab extends JTabbedPane implements ActionListener {
      */
     public CodeTextArea getSelectedPane() {
         return (CodeTextArea) getSelectedComponent();
+    }
+
+    /**
+     * Creates a title made up of a name and a close button
+     * @param title
+     * @return a JPanel
+     */
+    public JPanel makeTabTitle(String title) {
+        CloseButton tabButton = new CloseButton();
+        tabButton.setPreferredSize(new Dimension(20,20));
+        tabButton.addActionListener(e1 -> { removeTap(title);setSelectedIndex(getTabCount() - 2);});
+        JPanel pnl = new JPanel();
+        pnl.setLayout(new FlowLayout());
+        pnl.setOpaque(false);
+        JLabel label=new JLabel(title);
+        label.setFont(new Font("Barlow", 0, 11));
+        pnl.add(label);
+        pnl.add(tabButton);
+        return pnl;
+    }
+
+    /**
+     * Changes only name of title
+     * @param numberOfClick
+     */
+    public void changeTitle(final int numberOfClick) {
+        if (numberOfClick == 2 && !"Main".equals(getTitleAt(getSelectedIndex())))  {
+            String result = (String) JOptionPane.showInputDialog(null,
+                    "Change TabName", "Change name",
+                    JOptionPane.PLAIN_MESSAGE, null, null,"Red");
+            if(result != null && result.length() > 0){
+                int indexTab = getSelectedIndex();
+                setTitleAt(indexTab, result);
+                setTabComponentAt(indexTab, makeTabTitle(result));
+            }
+        }
     }
 }
