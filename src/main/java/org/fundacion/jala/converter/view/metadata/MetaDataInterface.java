3ca19dc21915ff56169e2db95f3151da5201ac05
danielaSantaCruz <daniela2010.1d@gmail.com>
2021-04-21 13:38:14 -0400

    Fix some checkstyle errors

--- src/main/java/org/fundacion/jala/converter/view/metadata/MetaDataInterface.java
+++ src/main/java/org/fundacion/jala/converter/view/metadata/MetaDataInterface.java
@@ -4,7 +4,7 @@
  * This software is the confidential and proprietary information of Fundacion Jala
  * ("Confidential Information"). You shall not disclose such Confidential
  * Information and shall use it only in accordance with the terms of the
- * license agreement you entered into with Fundacion Jala
+ * license agreement you entered into with Fundacion Jala.
  */
 
 package org.fundacion.jala.converter.view.metadata;
