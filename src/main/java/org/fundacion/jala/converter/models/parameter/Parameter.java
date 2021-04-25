5d3035e3e57acbda681e40db90eb69bb71cd294f
cristianfchq <cristian.fchq@gmail.com>
2021-04-25 18:14:59 -0400

    adds code convention in the class Parameter.java

--- src/main/java/org/fundacion/jala/converter/models/parameter/Parameter.java
+++ src/main/java/org/fundacion/jala/converter/models/parameter/Parameter.java
@@ -7,13 +7,15 @@
  * license agreement you entered into with Fundacion Jala
  *
  * @author Juan Pablo Gonzales
- * @version 1.0
  */
 
 package org.fundacion.jala.converter.models.parameter;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * This class is the options of the java parameters.
+ */
 @Service
 public class Parameter {
     private String filePath;
@@ -26,16 +28,18 @@ public class Parameter {
     }
 
     /**
-     * Returns a string with the path
-     * @return the file's path
+     * Obtains a string with the path.
+     *
+     * @return the file's path.
      */
     public String getFilePath() {
         return filePath;
     }
 
     /**
-     * Sets the file's path
-     * @param newFilePath a string with the path
+     * Sets the new file's path.
+     *
+     * @param newFilePath a string with the path.
      */
     public void setFilePath(final String newFilePath) {
         this.filePath = newFilePath;
