6759406f2346eaf6a85ec243614a628fe7acb671
Gustavo Huanca <gustavozha@gmail.com>
2021-04-26 14:03:23 -0400

    Refactor and rename gitZipeFile to builtZipFile in ZipFileFacade class

--- src/main/java/org/fundacion/jala/converter/models/facade/ZipFileFacade.java
+++ src/main/java/org/fundacion/jala/converter/models/facade/ZipFileFacade.java
@@ -25,7 +25,7 @@ public class ZipFileFacade {
     }
 
     /**
-     * Compresses files of Audio
+     * Compresses files of Audio.
      *
      * @param parameterOutputChecksum object of ParameterOutputChecksum.
      * @param metadata if add metadata of audio into zip.
@@ -35,11 +35,11 @@ public class ZipFileFacade {
      */
     public static void getZipFileAudio(final ParameterOutputChecksum parameterOutputChecksum, final boolean metadata,
                                        final String outputFilename) throws IOException, InterruptedException {
-        getZipFile(parameterOutputChecksum, metadata, outputFilename, false);
+        builtZipFile(parameterOutputChecksum, metadata, outputFilename, false);
     }
 
     /**
-     * Compresses files of video.
+     * Compresses files of Video.
      *
      * @param parameterOutputChecksum object of ParameterOutputChecksum.
      * @param metadata if add metadata of video into zip.
@@ -51,11 +51,11 @@ public class ZipFileFacade {
     public static void getZipFileVideo(final ParameterOutputChecksum parameterOutputChecksum, final boolean metadata,
                                        final boolean thumbnail, final String outputFilename)
             throws IOException, InterruptedException {
-        getZipFile(parameterOutputChecksum, metadata, outputFilename, thumbnail);
+        builtZipFile(parameterOutputChecksum, metadata, outputFilename, thumbnail);
     }
 
     /**
-     * Compresses files.
+     * Builds the files in order to compress.
      *
      * @param parameterOutputChecksum object of ParameterOutputChecksum.
      * @param metadata if add metadata into zip.
@@ -64,36 +64,51 @@ public class ZipFileFacade {
      * @throws IOException when invalid path is given in zipFiles.
      * @throws InterruptedException is exception if process is interrupted.
      */
-    private static void getZipFile(final ParameterOutputChecksum parameterOutputChecksum, final boolean metadata,
-                                   final String outputFilename, final boolean thumbnail)
+    private static void builtZipFile(final ParameterOutputChecksum parameterOutputChecksum, final boolean metadata,
+                                     final String outputFilename, final boolean thumbnail)
             throws IOException, InterruptedException {
+        final int USER_ID = 1;
         String checksumLocal = parameterOutputChecksum.getChecksumLocal();
         String storagePath = parameterOutputChecksum.getOutputFilename();
         int resultTitleSize = parameterOutputChecksum.getResultTitleSize();
         String filename = parameterOutputChecksum.getFileName();
-        final int WAIT_TIME = 6000;
-        final int USER_ID = 1;
         String nameWithoutExtension = outputFilename.substring(0, outputFilename.lastIndexOf(".") + 1);
         String pathFile = storagePath.substring(0, storagePath.lastIndexOf(System.getProperty("file.separator")) + 1);
         if (!(resultTitleSize > 0)) {
             insertAssetData(filename, pathFile, checksumLocal, USER_ID);
         }
-        if (metadata || thumbnail) {
-            ArrayList<String> zipList = new ArrayList<>();
-            zipList.add(pathFile + outputFilename);
-            if (metadata) {
-                zipList.add(pathFile + nameWithoutExtension + "txt");
-            }
-            if (thumbnail) {
-                zipList.add(pathFile + nameWithoutExtension + "png");
-            }
-            Thread.sleep(WAIT_TIME);
-            zipFiles(zipList, pathFile + nameWithoutExtension + "zip");
-        } else {
+        createZip(metadata, thumbnail, pathFile, outputFilename, nameWithoutExtension);
+    }
+
+    /**
+     * Creates zip.
+     *
+     * @param metadata if add metadata into zip.
+     * @param thumbnail if add thumbnail of video into zip.
+     * @param pathFile is path where zip are downloaded
+     * @param outputFilename path of file to compress.
+     * @param nameWithoutExtension name's zip.
+     * @throws IOException when invalid path is given in zipFiles.
+     * @throws InterruptedException is exception if process is interrupted.
+     */
+    private static void createZip(final boolean metadata, final boolean thumbnail, final String pathFile,
+                                  final String outputFilename, final String nameWithoutExtension)
+            throws InterruptedException, IOException {
+        final int WAIT_TIME = 6000;
+        if (!metadata && !thumbnail) {
             Thread.sleep(WAIT_TIME);
             zipFile(pathFile + outputFilename, pathFile + nameWithoutExtension + "zip");
+            return;
         }
-        System.out.println("SS");
+        ArrayList<String> zipList = new ArrayList<>();
+        zipList.add(pathFile + outputFilename);
+        if (metadata) {
+            zipList.add(pathFile + nameWithoutExtension + "txt");
+        }
+        if (thumbnail) {
+            zipList.add(pathFile + nameWithoutExtension + "png");
+        }
+        Thread.sleep(WAIT_TIME);
+        zipFiles(zipList, pathFile + nameWithoutExtension + "zip");
     }
 }
-
