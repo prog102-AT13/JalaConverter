a6dca37b216d8747924a5b132f6882e2ff3856c4
JessickaMoya <jessicka.moya@gmail.com>
2021-04-27 15:20:24 -0400

    Create PaoPaoResponse class

---
+++ src/main/java/org/fundacion/jala/converter/response/PaoPaoResponse.java
@@ -0,0 +1,17 @@
+package org.fundacion.jala.converter.response;
+
+public class PaoPaoResponse {
+    private String status;
+
+    public PaoPaoResponse(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
