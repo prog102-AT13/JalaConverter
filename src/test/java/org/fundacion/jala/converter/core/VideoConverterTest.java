735ca1f9d1a8a7f2aecfd80f90985290e371658d
Raymundo Guaraguara <raymundogs@gmail.com>
2021-05-07 15:51:32 -0400

    add unit tests

--- src/test/java/org/fundacion/jala/converter/core/VideoConverterTest.java
+++ src/test/java/org/fundacion/jala/converter/core/VideoConverterTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertEquals;
 public class VideoConverterTest {
 
     @Test
-    public void shouldCreateConvertedFile() throws ConverterException {
+    public void shouldCreateConvertedFileWithThumbnail() throws ConverterException {
         String filePath = System.getProperty("user.dir")
                 + "/src/test/resources/archive";
         String videoPath = filePath + "/Perrito.mp4";
@@ -21,6 +21,66 @@ public class VideoConverterTest {
         String expected = "JalaConverter/src/test/resources/archive/Perrito.mpeg";
         File videoFile = new File(filePath + "/Perrito.mpeg");
         File thumbnailFile = new File(filePath + "/Perrito.png");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCreateConvertedFileWithoutThumbnail() throws ConverterException {
+        String filePath = System.getProperty("user.dir")
+                + "/src/test/resources/archive";
+        String videoPath = filePath + "/Perrito.mp4";
+        VideoParameter videoParameter = new VideoParameter(videoPath, "mpeg",
+                "720", false, 30, 1280, 720, false);
+        VideoConverter videoConverter = new VideoConverter(videoParameter);
+        videoConverter.convertVideo();
+        String actual = videoConverter.getOutputFileName();
+        String expected = "JalaConverter/src/test/resources/archive/Perrito.mpeg";
+        File videoFile = new File(filePath + "/Perrito.mpeg");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCreateConvertedFileWithFrameRateChanged() throws ConverterException {
+        String filePath = System.getProperty("user.dir")
+                + "/src/test/resources/archive";
+        String videoPath = filePath + "/Perrito.mp4";
+        VideoParameter videoParameter = new VideoParameter(videoPath, "mpeg",
+                "720", false, 60, 1280, 720, false);
+        VideoConverter videoConverter = new VideoConverter(videoParameter);
+        videoConverter.convertVideo();
+        String actual = videoConverter.getOutputFileName();
+        String expected = "JalaConverter/src/test/resources/archive/Perrito.mpeg";
+        File videoFile = new File(filePath + "/Perrito.mpeg");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCreateConvertedFileWithResolutionChanged() throws ConverterException {
+        String filePath = System.getProperty("user.dir")
+                + "/src/test/resources/archive";
+        String videoPath = filePath + "/Perrito.mp4";
+        VideoParameter videoParameter = new VideoParameter(videoPath, "mpeg",
+                "720", false, 30, 720, 360, false);
+        VideoConverter videoConverter = new VideoConverter(videoParameter);
+        videoConverter.convertVideo();
+        String actual = videoConverter.getOutputFileName();
+        String expected = "JalaConverter/src/test/resources/archive/Perrito.mpeg";
+        File videoFile = new File(filePath + "/Perrito.mpeg");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCreateConvertedFileWithoutAudio() throws ConverterException {
+        String filePath = System.getProperty("user.dir")
+                + "/src/test/resources/archive";
+        String videoPath = filePath + "/Perrito.mp4";
+        VideoParameter videoParameter = new VideoParameter(videoPath, "mpeg",
+                "720", false, 30, 720, 360, true);
+        VideoConverter videoConverter = new VideoConverter(videoParameter);
+        videoConverter.convertVideo();
+        String actual = videoConverter.getOutputFileName();
+        String expected = "JalaConverter/src/test/resources/archive/Perrito.mpeg";
+        File videoFile = new File(filePath + "/Perrito.mpeg");
         assertEquals(expected, actual);
     }
 
