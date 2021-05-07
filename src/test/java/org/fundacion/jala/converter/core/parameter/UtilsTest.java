232c84712bafe50c4b15d483695034a7706b1d2d
Joel Rojas <jr.bestone17@gmail.com>
2021-05-07 16:26:24 -0400

    Correct checkstyle mistakes

--- src/test/java/org/fundacion/jala/converter/core/parameter/UtilsTest.java
+++ src/test/java/org/fundacion/jala/converter/core/parameter/UtilsTest.java
@@ -10,29 +10,29 @@ import static org.junit.Assert.assertEquals;
 public class UtilsTest {
 
     @Before
-    public void createFile() {
+    public void itShouldCreateFile() {
         RunCommand runCommand = new RunCommand();
         runCommand.run("cd archive && dir > emptyFile.txt");
     }
 
     @After
-    public void deleteFile() {
+    public void itShouldDeleteFile() {
         RunCommand runCommand = new RunCommand();
         runCommand.run("cd archive && del emptyFile2.txt");
     }
 
     @Test
-    public void cleanFileNameParameter() {
+    public void itShouldCleanFileNameParameter() {
         assertEquals("cleanFileName.txt", Utils.cleanFileNameParameter("cleanFileName.txt"));
     }
 
     @Test
-    public void cleanFileNameParameterWithSpace() {
+    public void itShouldCleanFileNameParameterWithSpace() {
         assertEquals("cleanFileName.txt", Utils.cleanFileNameParameter("cleanFile Name.txt"));
     }
 
     @Test
-    public void changeNameFile() {
+    public void itShouldChangeNameFile() {
         Utils.changeNameFile("emptyFile.txt", "emptyFile2.txt");
     }
 }
