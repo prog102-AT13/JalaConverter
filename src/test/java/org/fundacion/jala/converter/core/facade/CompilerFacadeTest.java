43362d0c41b8cac0f128f6d713a7c755a61b105c
Joel Rojas <jr.bestone17@gmail.com>
2021-05-07 12:35:28 -0400

    Add only necesary imports

--- src/test/java/org/fundacion/jala/converter/core/facade/CompilerFacadeTest.java
+++ src/test/java/org/fundacion/jala/converter/core/facade/CompilerFacadeTest.java
@@ -2,9 +2,8 @@ package org.fundacion.jala.converter.core.facade;
 
 import org.fundacion.jala.converter.core.exceptions.CompilerException;
 import org.junit.Test;
-
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class CompilerFacadeTest {
 
