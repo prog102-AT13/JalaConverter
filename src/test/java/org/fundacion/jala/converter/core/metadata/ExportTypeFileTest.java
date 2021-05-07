29bfe78e4cf5ddaa55811d43d7f9805439294a6d
Juan Pablo Gonzales <juampi7237@gmail.com>
2021-05-06 22:03:26 -0400

    test ExportTypeFileTest

---
+++ src/test/java/org/fundacion/jala/converter/core/metadata/ExportTypeFileTest.java
@@ -0,0 +1,30 @@
+package org.fundacion.jala.converter.core.metadata;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.io.File;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExportTypeFileTest {
+
+    @Test
+    public void getNameFileCompleteToExport() {
+        ExportTypeFile exportTypeFile = Mockito.mock(ExportTypeFile.class);
+        Mockito.when(exportTypeFile.getNameFileCompleteToExport()).thenReturn("fileName");
+        String actual = exportTypeFile.getNameFileCompleteToExport();
+        String expected = "fileName";
+        assertEquals(expected, actual);
+    }
+
+
+
+    @Test
+    public void getEmptyNameExportTypeFile() {
+        ExportTypeFile exportTypeFile = new ExportTypeFile("","",TypeFileExport.TXT,new File("path"));
+        String actual = exportTypeFile.getNameFileExport();
+        String expected = "";
+        assertEquals(expected, actual);
+    }
+}
