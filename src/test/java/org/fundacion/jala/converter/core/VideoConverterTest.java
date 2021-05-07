51e22612785afaa30034183d743fd616974a4b55
Raymundo Guaraguara <raymundogs@gmail.com>
2021-05-07 14:43:27 -0400

    correct videoconverter

--- src/test/java/org/fundacion/jala/converter/core/VideoConverterTest.java
+++ src/test/java/org/fundacion/jala/converter/core/VideoConverterTest.java
@@ -2,6 +2,7 @@ package org.fundacion.jala.converter.core;
 
 import org.fundacion.jala.converter.core.exceptions.ConverterException;
 import org.fundacion.jala.converter.core.parameter.VideoParameter;
+import org.junit.AfterClass;
 import org.junit.Test;
 
 import java.io.File;
@@ -10,16 +11,32 @@ import static org.junit.Assert.*;
 
 public class VideoConverterTest {
 
+    private static String rutePath() {
+        String path = System.getProperty("user.dir");
+        String pathResource = "\\archive";
+        String resourceDir = path + File.separator + pathResource + File.separator;
+        return resourceDir;
+    }
+
+    @AfterClass
+    public static void deleteFile() {
+        File audioTest = new File(rutePath() + "audioTest.avi");
+        audioTest.delete();
+    }
+
     @Test
     public void convertVideoShouldCreateConvertedFile() throws ConverterException {
         String filePath = System.getProperty("user.dir")
-                + "/src/test/archive/Perrito.mp4";
-        VideoParameter videoParameter = new VideoParameter(new File(filePath).getAbsolutePath(), "mpeg",
+                + "/src/test/resources/archive";
+        String videoPath = filePath + "/Perrito.mp4";
+        VideoParameter videoParameter = new VideoParameter(videoPath, "mpeg",
                 "720", true, 30, 1280, 720, false);
         VideoConverter videoConverter = new VideoConverter(videoParameter);
         videoConverter.convertVideo();
         String actual = videoConverter.getOutputFileName();
-        String expected = "Perrito.mpeg";
+        String expected = "JalaConverter/src/test/resources/archive/Perrito.mpeg";
+        File videoFile = new File(filePath + "/Perrito.mpeg");
+        File thumbnailFile = new File(filePath + "/Perrito.png");
         assertEquals(expected, actual);
     }
 
