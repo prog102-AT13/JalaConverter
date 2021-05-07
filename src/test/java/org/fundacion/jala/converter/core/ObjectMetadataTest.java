28bb5ef0fb3561dc61dd32e6363688ab3b276dbc
Juan Pablo Gonzales <juampi7237@gmail.com>
2021-05-06 23:05:05 -0400

    delete space between import lines

--- src/test/java/org/fundacion/jala/converter/core/ObjectMetadataTest.java
+++ src/test/java/org/fundacion/jala/converter/core/ObjectMetadataTest.java
@@ -5,7 +5,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 import java.io.File;
-
 import static org.junit.Assert.assertEquals;
 
 public class ObjectMetadataTest {
