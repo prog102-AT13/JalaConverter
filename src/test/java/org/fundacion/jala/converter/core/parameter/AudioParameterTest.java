bbb3e11ac0f58648487b4713346117a77c25d5a0
SaulCM <saulcaspamiranda@gmail.com>
2021-05-07 09:54:09 -0400

    added tests for the AudioParameter class

--- src/test/java/org/fundacion/jala/converter/core/parameter/AudioParameterTest.java
+++ src/test/java/org/fundacion/jala/converter/core/parameter/AudioParameterTest.java
@@ -17,6 +17,15 @@ import static org.junit.Assert.*;
  */
 
 public class AudioParameterTest {
+
+    @Test
+    public void ItShouldReturnVariables() {
+        AudioParameter audioParameter = new AudioParameter("path", "format", "bitrate", "frequency", "volume", "audioChannels");
+        String expected = "format";
+        String actual = audioParameter.getFormat();
+        assertEquals(expected, actual);
+    }
+
     @Test
     public void getFilepathTest() {
         AudioParameter audioParameter = new AudioParameter();
@@ -25,12 +34,139 @@ public class AudioParameterTest {
         String actual = audioParameter.getFilePath();
         assertEquals(expected, actual);
     }
+
     @Test
-    public void formatVolumeTest() {
+    public void ItShouldReturnFormatVolumeCommand() {
         AudioParameter audioParameter = new AudioParameter();
         audioParameter.setVolume("2");
         String expected = " -filter:a \"volume=2\" ";
         String actual = audioParameter.formatVolume();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ItShouldReturnEmptyFrequencyCommand() {
+        AudioParameter audioParameter = new AudioParameter();
+        audioParameter.setFrequency("");
+        String expected = "";
+        String actual = audioParameter.formatHz();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ItShouldReturnLowFrequencyCommand() {
+        AudioParameter audioParameter = new AudioParameter();
+        audioParameter.setFrequency("20");
+        String expected = " -ar 22050 ";
+        String actual = audioParameter.formatHz();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ItShouldReturnMediumFrequencyCommand() {
+        AudioParameter audioParameter = new AudioParameter();
+        audioParameter.setFrequency("44");
+        String expected = " -ar 44100 ";
+        String actual = audioParameter.formatHz();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ItShouldReturnHighFrequencyCommand() {
+        AudioParameter audioParameter = new AudioParameter();
+        audioParameter.setFrequency("48");
+        String expected = " -ar 48000 ";
+        String actual = audioParameter.formatHz();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ItShouldReturnFormatBitrateCommand() {
+        AudioParameter audioParameter = new AudioParameter();
+        audioParameter.setBitrate("64");
+        String expected = " -ab " + 64 * 1000 +" ";
+        String actual = audioParameter.formatBitrate();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ItShouldReturnEmptyAudioChannelCommand() {
+        AudioParameter audioParameter = new AudioParameter();
+        audioParameter.setAudioChannels("unavailable");
+        String expected = "";
+        String actual = audioParameter.formatAudioChannel();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ItShouldReturnOneChannelCommand() {
+        AudioParameter audioParameter = new AudioParameter();
+        audioParameter.setAudioChannels("mono");
+        String expected = " -ac 1 ";
+        String actual = audioParameter.formatAudioChannel();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ItShouldReturnTwoChannelCommand() {
+        AudioParameter audioParameter = new AudioParameter();
+        audioParameter.setAudioChannels("stereo");
+        String expected = " -ac 2 ";
+        String actual = audioParameter.formatAudioChannel();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ItShouldReturnThreeChannelCommand() {
+        AudioParameter audioParameter = new AudioParameter();
+        audioParameter.setAudioChannels("2.1");
+        String expected = " -ac 3 ";
+        String actual = audioParameter.formatAudioChannel();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ItShouldReturnFourChannelCommand() {
+        AudioParameter audioParameter = new AudioParameter();
+        audioParameter.setAudioChannels("4.0");
+        String expected = " -ac 4 ";
+        String actual = audioParameter.formatAudioChannel();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ItShouldReturnFiveChannelCommand() {
+        AudioParameter audioParameter = new AudioParameter();
+        audioParameter.setAudioChannels("5.0");
+        String expected = " -ac 5 ";
+        String actual = audioParameter.formatAudioChannel();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ItShouldReturnSixChannelCommand() {
+        AudioParameter audioParameter = new AudioParameter();
+        audioParameter.setAudioChannels("5.1");
+        String expected = " -ac 6 ";
+        String actual = audioParameter.formatAudioChannel();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ItShouldReturnSevenChannelCommand() {
+        AudioParameter audioParameter = new AudioParameter();
+        audioParameter.setAudioChannels("6.1");
+        String expected = " -ac 7 ";
+        String actual = audioParameter.formatAudioChannel();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ItShouldReturnEightChannelCommand() {
+        AudioParameter audioParameter = new AudioParameter();
+        audioParameter.setAudioChannels("7.1");
+        String expected = " -ac 8 ";
+        String actual = audioParameter.formatAudioChannel();
         assertEquals(expected, actual);
     }
 }
